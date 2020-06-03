d9739a1d741b27ac90a0c16438e6d5a6f0d9f019
Kashish Madan <kashish.madan@outlook.com>
2020-06-03 22:51:56 +0530

    UDP files added

---
+++ UDP_server.py
@@ -0,0 +1,11 @@
+import socket
+
+UDP_IP_ADDRESS = "192.168.243.1"
+UDP_PORT_NO = 20001
+serverSock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+
+serverSock.bind((UDP_IP_ADDRESS, UDP_PORT_NO))
+while True:
+    data, addr = serverSock.recvfrom(1024)
+    print ("Message: ")
+    print(data)
